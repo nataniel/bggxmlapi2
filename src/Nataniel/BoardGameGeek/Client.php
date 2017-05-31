a35e8abcb9edf9e57b9e3460ff595c4b2ebefe7c
Artur Jedliński <artur@jedlinski.pl>
2017-05-31 22:51:26 +0200

    Create Client.php

---
+++ src/Nataniel/BoardGameGeek/Client.php
@@ -0,0 +1,12 @@
+<?php
+namespace Nataniel\BoardGameGeek;
+
+/**
+ * Class Client
+ * @package Main\Services\BoardGameGeek
+ * https://boardgamegeek.com/wiki/page/BGG_XML_API2
+ */
+class Client
+{
+
+}
